510e2f6db0dba782167c50cd0acfb2370a71b6b9
Bartosz Jarmuz <bartosz.jarmuz@gmail.com>
2018-06-11 11:40:12 +0200

    Add solution

---
+++ DbIntegrationTestHelpers/DbIntegrationTestHelpers/DropCreateDatabaseAlwaysAndSeed.cs
@@ -0,0 +1,25 @@
+﻿namespace DbIntegrationTestHelpers
+{
+    using System.Data.Entity;
+
+    internal sealed class DropCreateDatabaseAlwaysAndSeed : DropCreateDatabaseAlways<DbContext>
+    {
+        public override void InitializeDatabase(DbContext context)
+        {
+            // Close all connections before drop
+            if (context.Database.Exists())
+            {
+                context.Database.ExecuteSqlCommand(
+                    TransactionalBehavior.DoNotEnsureTransaction,
+                    $"ALTER DATABASE [{context.Database.Connection.Database}] SET SINGLE_USER WITH ROLLBACK IMMEDIATE");
+            }
+
+            base.InitializeDatabase(context);
+        }
+
+        protected override void Seed(DbContext context)
+        {
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DbIntegrationTestHelpers/DbIntegrationTestHelpers/StaticContextIntegrationsTestsBase.cs
@@ -0,0 +1,64 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="IntegrationsTestsBase.cs" company="SDL plc">
+//   Copyright (c) SDL plc. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace DbIntegrationTestHelpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    using System.Linq;
+    using NUnit.Framework;
+
+    public class StaticContextIntegrationsTestsBase<T> where T : DbContext
+    {
+        protected StaticContextIntegrationsTestsBase()
+        {
+        }
+
+        private const string ConnectionString =
+            @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=telimena-AutomatedTestsDb;Integrated Security=SSPI;MultipleActiveResultSets=true;";
+
+        private static readonly T ContextBackingField = StaticContextIntegrationsTestsBase<T>.GetContextInstance<T>();
+        public T Context => StaticContextIntegrationsTestsBase<T>.ContextBackingField;
+
+        public static T GetContextInstance<T>() where T : DbContext
+        {
+            try
+            {
+                var type = typeof(T);
+                var ctx = (T) Activator.CreateInstance(type, StaticContextIntegrationsTestsBase<T>.ConnectionString);
+
+                return ctx;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Your DB context has to have a constructor which allows setting a connection string", ex);
+            }
+        }
+
+        [TearDown]
+        public void ResetChangeTracker()
+        {
+            IEnumerable<DbEntityEntry> changedEntriesCopy = StaticContextIntegrationsTestsBase<T>.ContextBackingField.ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Added ||
+                            e.State == EntityState.Modified ||
+                            e.State == EntityState.Deleted
+                );
+            foreach (DbEntityEntry entity in changedEntriesCopy)
+            {
+                StaticContextIntegrationsTestsBase<T>.ContextBackingField.Entry(entity.Entity).State = EntityState.Detached;
+            }
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Database.SetInitializer(new DropCreateDatabaseAlwaysAndSeed());
+            StaticContextIntegrationsTestsBase<T>.ContextBackingField.Database.Initialize(false);
+        }
+    }
+}
